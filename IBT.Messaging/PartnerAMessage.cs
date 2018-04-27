b41d7a526740aa2c9a0b167dcfb852b22f3a5a71
George Barbu <georgebarbu@live.com>
2018-04-27 23:25:14 +0100

    Added initial messages.

---
+++ IBT.Messaging/PartnerAMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBT.Messaging
+{
+    public sealed class PartnerAMessage
+    {
+        public string ProductNameFull { get; set; }
+        public int IBTTypeCode { get; set; }
+        public int EventType { get; set; }
+        public string ISIN { get; set; }
+    }
+}
---
+++ IBT.Messaging/PartnerBMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace IBT.Messaging
+{
+    public sealed class PartnerBMessage
+    {
+        public double TimeSpan { get; set; }
+        public string ISIN { get; set; }
+    }
+}
